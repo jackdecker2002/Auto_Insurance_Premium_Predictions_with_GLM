5eadcfd299370473b9412e6de2cd575fae2777d0
jackdecker2002 <jackdecker2002@gmail.com>
2025-08-25 16:22:01 -0400

    August 25 Work Update

--- DataPreProcessing.sql
+++ DataPreProcessing.sql
@@ -68,7 +68,15 @@ ORDER BY cnt DESC;
 
 -- Phase 2: Aggregate queries
 
--- Sort by Policies, Insureds, Incidents, Claims, and Vehicles
+-- Sort by Policies, Insureds, Incidents and Vehicles
+
+/*
+DROP TABLE IF EXISTS insureds;
+DROP TABLE IF EXISTS policies;
+DROP TABLE IF EXISTS incidents;
+DROP TABLE IF EXISTS vehicles;
+*/
+
 
 CREATE TABLE policies AS
 SELECT DISTINCT
@@ -84,39 +92,65 @@ FROM RawData;
 
 CREATE TABLE insureds AS
 SELECT DISTINCT
+    ROW_NUMBER() OVER () AS insured_id,
     policy_number,
     age,
     insured_zip,
     insured_sex,
     insured_education_level,
-    insured_income,
     insured_occupation,
-    insured_hobbies
+    insured_hobbies,
+    insured_relationship,
+    "capital-gains",
+    "capital-loss"
 FROM RawData;
 
 CREATE TABLE incidents AS
-SELECT DISTINCT
-    incident_id,
+SELECT
+    ROW_NUMBER() OVER () AS incident_id,
+    policy_number,
     incident_date,
     incident_type,
-    incident_description,
-    incident_severity
-FROM RawData;
-
-CREATE TABLE claims AS
-SELECT DISTINCT
-    claim_id,
-    claim_date,
-    claim_amount,
-    claim_status,
-    claim_description
+    collision_type,
+    incident_severity,
+    authorities_contacted,
+    incident_state,
+    incident_city,
+    incident_location,
+    incident_hour_of_the_day,
+    number_of_vehicles_involved,
+    property_damage,
+    bodily_injuries,
+    witnesses,
+    police_report_available,
+    total_claim_amount,
+    injury_claim,
+    property_claim,
+    vehicle_claim,
+    fraud_reported
 FROM RawData;
 
 CREATE TABLE vehicles AS
 SELECT DISTINCT
-    vehicle_id,
-    vehicle_make,
-    vehicle_model,
-    vehicle_year,
-    vehicle_type
+    ROW_NUMBER() OVER () AS vehicle_id,
+    policy_number,
+    auto_make,
+    auto_model,
+    auto_year
 FROM RawData;
+*/
+
+SELECT p.policy_number, i.insured_id, i.age, i.insured_occupation
+FROM policies p
+JOIN insureds i ON p.policy_number = i.policy_number
+LIMIT 10;
+
+SELECT p.policy_number, inc.incident_id, inc.incident_date, inc.total_claim_amount
+FROM policies p
+JOIN incidents inc ON p.policy_number = inc.policy_number
+LIMIT 10;
+
+SELECT p.policy_number, v.vehicle_id, v.auto_make, v.auto_model, v.auto_year
+FROM policies p
+JOIN vehicles v ON p.policy_number = v.policy_number
+LIMIT 10;
